9a40f90c94bc3082884eba34d3e94e9c3bd99844
Victor012606 <100692708+Victor012606@users.noreply.github.com>
2022-03-02 02:02:19 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -4,7 +4,7 @@ let targetImage = document.querySelector("#smart-image");
     targetImage.classList.remove("small");
    }); 
   if (targetImage.classList.contains("small")) {
-   targetImage.classList.add("small");
+   targetImage.classList.remove("small");
 } else {
    targetImage.classList.add("small");
 }
