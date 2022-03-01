e48aed27036d0ceb1893f94ccac67bfb53a8b47c
Victor012606 <100692708+Victor012606@users.noreply.github.com>
2022-03-02 01:58:32 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,4 +3,9 @@ let targetImage = document.querySelector("#smart-image");
   targetImage.addEventListener("click", function() {
     targetImage.classList.remove("small");
    }); 
+  if (targetImage.classList.contains("small")) {
+   targetImage.classList.add("small");
+} else {
+   targetImage.classList.add("small");
+}
 });
